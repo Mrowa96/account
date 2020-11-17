3e1c97eb16bf135093d24932f5e94a67b2b79a1f
Paweł Mrowiec <work@pawel-mrowiec.pl>
2020-11-17 19:33:14 +0100

    Cleanup

--- src/components/Box/Box.types.ts
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from 'react';
-
-export type BoxPropsType = {
-  title: string;
-  uniqueName: string;
-  children: ReactNode;
-  isExpandedByDefault?: boolean;
-  className?: string;
-};
