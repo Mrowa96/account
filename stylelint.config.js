bb3d2c7aeaf1170a1749779fc718afc96e779c7c
Paweł Mrowiec <contact@pawel-mrowiec.dev>
2020-11-23 18:03:15 +0100

    Update dependencies (#6)
    * Update postcss and related
    
    * Update styles related packages
    
    * Try to update webpack - failed - and update fastify-cors
    
    Co-authored-by: Paweł Mrowiec <work@pawel-mrowiec.pl>

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: 'stylelint-prettier/recommended',
+  extends: ['stylelint-config-css-modules', 'stylelint-prettier/recommended'],
   plugins: ['stylelint-scss', 'stylelint-order'],
   rules: {
     'order/properties-alphabetical-order': true,
